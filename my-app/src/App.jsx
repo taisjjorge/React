1c093e0eb7f9f5a695e491488fd35bec930188b7
tais.amanda <tais.amanda@g.globo>
2024-02-16 16:39:59 -0300

    feat: Utilizando hook de efeito para sincronizar componente com dados de api externa

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,32 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios"
+import { ThreeDots } from 'react-loader-spinner'
 
 import "./styles/App.css";
-
-import articleImg from "./assets/images/gatinho1.png"
-import articleImg2 from "./assets/images/gatinho2.png"
 
 import { NavBar } from "./components/Navbar/Navbar";
 import { Article } from "./components/Article/Article";
 import { Counter } from "./components/Counter/Counter";
 
 function App() {
+  const [news, setNews] = useState([])
+  
+  useEffect(() => {
+    async function loadNews(){
+      const response = await axios.get('https://api.spaceflightnewsapi.net/v3/articles')
+      const newsData = response.data
+
+      setNews(newsData)
+    }
+    loadNews()
+  }, [])
+
   return (
     <>
       <NavBar />
       {/* <Counter /> */}
       <section id="articles">
-        <Article 
-          title={'New title'}
-          provider='Author'
-          description='Morbi lobortis, purus a convallis cursus, augue eros finibus justo, at maximus erat lorem sed diam. Fusce molestie volutpat elit, non imperdiet risus finibus ut. Proin eget dapibus erat, et tempor nisi. Nullam non tempus lorem, id convallis velit. Donec scelerisque nisl sit amet semper hendrerit. Ut quis facilisis nisi, sed viverra quam. Donec gravida elit est, sed venenatis leo porta at. Fusce volutpat tellus quis euismod dapibus. Mauris in erat arcu. Duis nec sodales felis. Vestibulum at elit varius, elementum elit eu, rhoncus diam. Sed viverra gravida ex, sit amet pretium neque sodales eu. Suspendisse ornare, nisl vel porta convallis, velit leo volutpat nulla, at accumsan massa mauris eget sem. Sed sed ex scelerisque, facilisis nulla a, cursus lectus. In porta libero in viverra dignissim. Suspendisse bibendum dapibus velit, ut malesuada nisi tempus sed.'
-          thumbnail={articleImg}
-        />
-        <Article 
-          title={'Another another title'}
-          provider='Author 2'
-          description='Morbi lobortis, purus a convallis cursus, augue eros finibus justo, at maximus erat lorem sed diam. Fusce molestie volutpat elit, non imperdiet risus finibus ut. Proin eget dapibus erat, et tempor nisi. Nullam non tempus lorem, id convallis velit. Donec scelerisque nisl sit amet semper hendrerit. Ut quis facilisis nisi, sed viverra quam. Donec gravida elit est, sed venenatis leo porta at. Fusce volutpat tellus quis euismod dapibus. Mauris in erat arcu. Duis nec sodales felis. Vestibulum at elit varius, elementum elit eu, rhoncus diam. Sed viverra gravida ex, sit amet pretium neque sodales eu. Suspendisse ornare, nisl vel porta convallis, velit leo volutpat nulla, at accumsan massa mauris eget sem. Sed sed ex scelerisque, facilisis nulla a, cursus lectus. In porta libero in viverra dignissim. Suspendisse bibendum dapibus velit, ut malesuada nisi tempus sed.'
-          thumbnail={articleImg2}
-        />
+        {news.length === 0 ? (
+          <div style={{ 
+              height: 400, 
+              width: '100%', 
+              display: 'flex', 
+              justifyContent: 'center', 
+              alignItems: 'center' 
+          }}>
+            <ThreeDots
+              height="80"
+              width="80"
+              radius="9"
+              color="white"
+              ariaLabel="loading"
+              wrapperStyle={{ margin: "auto" }}
+              wrapperClass="loader"
+            />
+          </div>
+        ) : news.map((article, index) => {
+          return (
+            <Article 
+              key={index}
+              title={article.title}
+              provider={article.newsSite}
+              description={article.summary}
+              thumbnail={article.imageUrl}
+            />
+        )})}
       </section>
     </>
   );
